5984871b89fc90ea461a79d0439ad6fd1ab4e279
Atanas Dimov <nasko312@gmail.com>
2021-06-28 22:12:16 +0300

    added exceptions in push and pop

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import ds.stack.Stack;
 
 public class Main {
-    public static void main(String args[]){
-        Stack myStack = new Stack(1);
-        myStack.push(5);
-        System.out.println(myStack.peak());
-        myStack.push(10);
-        System.out.println(myStack.peak());
-        System.out.println("Pop: " + myStack.pop());
-        myStack.push(23);
-        System.out.println("Loop");
-        while (!myStack.isEmpty()){
-            System.out.println(myStack.pop());
+    public static void main(String[] args){
+        try {
+            Stack myStack = new Stack(1);
+            myStack.push(5);
+            System.out.println(myStack.peak());
+            myStack.push(6);
+
+            System.out.println("Loop");
+            while (!myStack.isEmpty()){
+                System.out.println(myStack.pop());
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/ds/stack/Stack.java
+++ src/ds/stack/Stack.java
@@ -5,42 +5,45 @@ public class Stack {
     private long[] stack_array;
     private int top;
 
-    public Stack(int size){
+    public Stack(int size) {
         this.maxSize = size;
         this.top = -1;
         stack_array = new long[size];
     }
+
     //push
-    public void push(long num){
-        if (this.isFull()){
-            System.out.println("The stack is full!");
-        }else{
+    public void push(long num) throws Exception {
+        if (this.isFull()) {
+            throw new Exception("The stack is full!");
+        } else {
             top++;
             stack_array[top] = num;
         }
     }
+
     //pop
-    public long pop(){
-        if (this.isEmpty()){
-            System.out.println("The stack is empty!");
-            return -1;//TODO use exception
-        }else {
-            int old_top = top;
-            top--;
-            return stack_array[old_top];
+    public long pop() throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("The stack is empty!");
         }
+        int old_top = top;
+        top--;
+        return stack_array[old_top];
     }
+
     //peak
-    public long peak(){
+    public long peak() {
         return stack_array[top];
     }
+
     //isEmpty
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return (top == -1);
     }
+
     //isFull
-    public boolean isFull(){
-        return (top == maxSize-1);
+    public boolean isFull() {
+        return (top == maxSize - 1);
     }
 
 }
